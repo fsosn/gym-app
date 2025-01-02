df3941ef4387a0c6c11bed67ab385800c69a3e05
Filip Sosnowski <filip.srebrna@gmail.com>
2025-01-02 13:41:10 +0100

    feat: add routine deletion

--- frontend/src/components/workout/routine/RoutineCreateUpdateForm.tsx
+++ frontend/src/components/workout/routine/RoutineCreateUpdateForm.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import { ArrowLeft, Plus } from "lucide-react";
+import { ArrowLeft, Plus, TrashIcon } from "lucide-react";
 import {
     Dialog,
     DialogContent,
@@ -11,6 +11,17 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -39,7 +50,6 @@ export function RoutineCreateUpdateForm() {
                     const routine = await axios.get(
                         `${API_ENDPOINTS.BASE_URL}${API_ENDPOINTS.ROUTINES}/${routineId}`
                     );
-                    console.log(routine.data);
                     setDisplayedTitle(routine.data.title);
                     setTitle(routine.data.title);
                     setDescription(routine.data.description);
@@ -94,9 +104,6 @@ export function RoutineCreateUpdateForm() {
             return;
         }
 
-        console.log(exercises);
-        console.log(title);
-
         const routineData = {
             title,
             description,
@@ -110,7 +117,6 @@ export function RoutineCreateUpdateForm() {
                 })),
             })),
         };
-        console.log(routineData);
 
         try {
             if (isUpdateMode) {
@@ -126,8 +132,21 @@ export function RoutineCreateUpdateForm() {
             }
             navigate("/");
         } catch (error) {
-            console.error("Error while saving workout:", error);
-            alert("Error while saving workout.");
+            console.error("Error while modifying routine:", error);
+            alert("Error while modifying routine.");
+        }
+    };
+
+    const handleDeleteRoutineButtonClick = async () => {
+        try {
+            await axios.delete(
+                `${API_ENDPOINTS.BASE_URL}${API_ENDPOINTS.ROUTINES}/${routineId}`
+            );
+
+            navigate("/");
+        } catch (error) {
+            console.error("Error while deleting routine:", error);
+            alert("Error while deleting routine.");
         }
     };
 
@@ -148,59 +167,66 @@ export function RoutineCreateUpdateForm() {
                 <h1 className="text-xl font-bold text-zinc-950 dark:text-zinc-100">
                     {isUpdateMode ? displayedTitle : "Routine"}
                 </h1>
-                <Dialog>
-                    <DialogTrigger asChild>
-                        <Button disabled={exercises?.length === 0}>
-                            {isUpdateMode ? "Update" : "Create"}
-                        </Button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                        <DialogHeader>
-                            <DialogTitle>
-                                {isUpdateMode ? "Update" : "Create"} Routine
-                            </DialogTitle>
-                            <DialogDescription>
-                                Please add a title and/or description for your
-                                routine. Click save when you're done.
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="title" className="text-right">
-                                    Title
-                                </Label>
-                                <Input
-                                    id="title"
-                                    className="col-span-3"
-                                    value={title}
-                                    onChange={(e) => setTitle(e.target.value)}
-                                />
-                                <Label
-                                    htmlFor="description"
-                                    className="text-right"
-                                >
-                                    Description
-                                </Label>
-                                <Input
-                                    id="description"
-                                    className="col-span-3"
-                                    value={description}
-                                    onChange={(e) =>
-                                        setDescription(e.target.value)
-                                    }
-                                />
-                            </div>
-                        </div>
-                        <DialogFooter>
-                            <Button
-                                type="button"
-                                onClick={handleCreateOrUpdateButtonClick}
-                            >
-                                Save
+                <div>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button disabled={exercises?.length === 0}>
+                                {isUpdateMode ? "Update" : "Create"}
                             </Button>
-                        </DialogFooter>
-                    </DialogContent>
-                </Dialog>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px]">
+                            <DialogHeader>
+                                <DialogTitle>
+                                    {isUpdateMode ? "Update" : "Create"} Routine
+                                </DialogTitle>
+                                <DialogDescription>
+                                    Please add a title and/or description for
+                                    your routine. Click save when you're done.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <div className="grid gap-4 py-4">
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label
+                                        htmlFor="title"
+                                        className="text-right"
+                                    >
+                                        Title
+                                    </Label>
+                                    <Input
+                                        id="title"
+                                        className="col-span-3"
+                                        value={title}
+                                        onChange={(e) =>
+                                            setTitle(e.target.value)
+                                        }
+                                    />
+                                    <Label
+                                        htmlFor="description"
+                                        className="text-right"
+                                    >
+                                        Description
+                                    </Label>
+                                    <Input
+                                        id="description"
+                                        className="col-span-3"
+                                        value={description}
+                                        onChange={(e) =>
+                                            setDescription(e.target.value)
+                                        }
+                                    />
+                                </div>
+                            </div>
+                            <DialogFooter>
+                                <Button
+                                    type="button"
+                                    onClick={handleCreateOrUpdateButtonClick}
+                                >
+                                    Save
+                                </Button>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
+                </div>
             </nav>
             <div className="flex flex-col items-center">
                 <div className="w-full sm:max-w-md md:max-w-md lg:max-w-xl xl:max-w-xl mx-auto">
@@ -226,6 +252,45 @@ export function RoutineCreateUpdateForm() {
                             />
                         )}
                     </div>
+                    {isUpdateMode ? (
+                        <div className="m-2 mt-2 pb-6">
+                            <AlertDialog>
+                                <AlertDialogTrigger asChild>
+                                    <Button
+                                        variant="outline"
+                                        className="w-full text-destructive hover:text-destructive border-red-900"
+                                    >
+                                        <TrashIcon className="w-4 h-4 mr-1" />
+                                        <span>Discard Routine</span>
+                                    </Button>
+                                </AlertDialogTrigger>
+                                <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                        <AlertDialogTitle>
+                                            Are you sure?
+                                        </AlertDialogTitle>
+                                        <AlertDialogDescription>
+                                            Please remember that this action
+                                            cannot be undone.
+                                        </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                        <AlertDialogCancel>
+                                            Go Back
+                                        </AlertDialogCancel>
+                                        <AlertDialogAction
+                                            className="bg-red-800 text-white border-red-900 hover:bg-red-700"
+                                            onClick={
+                                                handleDeleteRoutineButtonClick
+                                            }
+                                        >
+                                            Discard Routine
+                                        </AlertDialogAction>
+                                    </AlertDialogFooter>
+                                </AlertDialogContent>
+                            </AlertDialog>
+                        </div>
+                    ) : null}
                 </div>
             </div>
         </div>
